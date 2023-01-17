ac686b19357482418d7c1a2e613a757372fb5445
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-17 23:54:39 +0530

    feat: move colorscheme installs to colorscheme.lua, change colorscheme to gruvbox

--- init.lua
+++ init.lua
@@ -49,7 +49,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme onedark]]
+vim.cmd [[colorscheme gruvbox]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,7 +19,6 @@ return {
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
 
-  'navarasu/onedark.nvim', -- Theme inspired by Atom
   'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
   'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
