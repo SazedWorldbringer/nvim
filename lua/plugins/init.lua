8207d88f37ce94dc28a77359f7cf1c554d86b762
Atharva Pardeshi <atharva.exe@gmail.com>
2023-01-17 23:47:37 +0530

    feat: move install and setup of treesitter plugins to treesitter.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,16 +19,6 @@ return {
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   },
 
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    }
-  },
-
   'navarasu/onedark.nvim', -- Theme inspired by Atom
   'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
   'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
